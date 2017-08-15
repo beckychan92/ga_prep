c09867d2e84dd638d1981200a48cd8cab7c5f234
Becky Chan <beckychan@Beckys-MacBook-Air.local>
2017-08-14 19:07:03 -0700

    almost done with bonus

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,18 +3,51 @@ console.log("Hi there, welcome to the JavaScript console!");
 
 // initialize the application
 var app = new Object();
+console.log("becky chan")
 
 app.shakePokeball = function shakePokeball() {
   // code BELOW this line for challenge #1 -->
-  
+  var pokeball = document.getElementById("pokeball");
+  pokeball.className = "shake";
+  console.log("shaking done!")
 } // <-- code ABOVE this line for challenge #1
 
 app.setName = function setName() {
   // code BELOW this line for challenge #2 -->
+  var name = prompt("Hello! What is your name?");
+  var nameElement = document.getElementById("first-name");
+  nameElement.innerHTML = name;
+
+  console.log("name is done! " + name)
 
 } // <-- code ABOVE this line for challenge #2
 
 app.setWebsite = function setWebsite() {
   // code BELOW this line for challenge #3 & bonus -->
+  var website = prompt("What is your favorite website?");
+  var webElement = document.getElementById("favorite-website")
+  webElement.innerHTML = website;
+  console.log("website done!" + website)
+
+  //check if site inputted includes "https://"
+  var httpStatus = website.slice(0, 7);
+  var http = "http://"
+  httpStatus !== "http://" ? website = http.concat(website) : website;
+  console.log(website)
+  //check if end of input includes ".com"
+  var comStatus = website.slice(-4, website[length -1]);
+  var com = ".com";
+  comStatus !== ".com" ? website = website.concat(com) : website;
+  //site ready for use 
+  console.log(website)
+
+
+
+
+  <a href=website>Lets go there!</a>
 
 } // <-- code ABOVE this line for challenge #3 & bonus
+
+
+
+
